0a5ac4db7b7741cce3a604f06fdb341c05758290
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2022-12-30 10:25:42 +0100

    3

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputChange = document.querySelector('input#font-size-control')
 const abracadabra = document.querySelector('span#text')
 
-inputChange.addEventListener("input", onControllerInput);
-function onControllerInput(event) {
+inputChange.addEventListener("input", onControllerInputChange);
+function onControllerInputChange(event) {
   return (abracadabra.style.fontSize = event.currentTarget.value + "px");
 }
